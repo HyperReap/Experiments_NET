9fef8ba8f0e7c8a6267cb8825c867440e3f2091b
Petr Urbánek <urbanek.vk@gmail.com>
2022-10-09 15:31:41 +0200

    constructor order clarification

---
+++ Constructors_order/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml.Linq;
+
+namespace Constructors_order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass c = new MyClass();
+
+        }
+    }
+
+
+    class MyClass : MyBaseClass
+    {
+        private string Name = "Name";
+        private static string  StatName = "StatName";
+        public MyClass()
+        {
+            Name = "ConstrName";
+        }
+        static MyClass()
+        {
+            StatName = "StaNameConstr";
+        }
+    }
+
+
+    class MyBaseClass
+    {
+        private string BaseName = "BaseName";
+        private static string BaseStatName = "BaseStatName";
+        public MyBaseClass()
+        {
+            BaseName = "BaseConstrName";
+
+        }
+        static MyBaseClass()
+        {
+            BaseStatName = "BaseStaticConstrName";
+        }
+    }
+
+}
